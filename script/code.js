96c3e204d87a40024b11089e178e8afd7a722806
Ismaaeel-A <ismaaeelahmed42@gmail.com>
2024-05-17 09:53:05 +0200

    complete

---
+++ script/code.js
@@ -0,0 +1,19 @@
+let obj = {
+    firstName: 'John',
+    lastName: 'Cena',
+    age: 20,
+    race: 'indian'
+}
+
+let obj1 = {
+    firstName: 'Nelson',
+    lastName: 'Mandela',
+    age: 67,
+    race: 'black'
+}
+
+console.log(obj)
+console.log(obj1)
+
+let arrList = [obj , obj1]
+console.log(arrList)
